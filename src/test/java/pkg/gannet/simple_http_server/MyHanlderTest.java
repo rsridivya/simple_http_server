e81496b2d7deb8205d99184a4378ee536ae86322
rsridivya <sridivya352@gmail.com>
2018-01-14 22:42:56 -0500

    get code coverage more

--- src/test/java/pkg/gannet/simple_http_server/MyHanlderTest.java
+++ src/test/java/pkg/gannet/simple_http_server/MyHanlderTest.java
@@ -1,6 +1,9 @@
 package pkg.gannet.simple_http_server;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
@@ -54,6 +57,16 @@ public class MyHanlderTest {
 		server.processParameters(queryParams,inputString);
 		Assert.assertEquals("Hello World\nNo Query Parameters Provided",server.getFinalString());
 	}
+	
+	@Test
+	public void checkProcessParametersSetFinalStringifInvalid() throws IOException {
+		MyHandler server = new MyHandler();
+		server.setFinalString("");
+		Map<String,String> queryParams = new HashMap();
+		String inputString = "Demo";
+		server.processParameters(queryParams,inputString);
+		Assert.assertEquals("Invalid Request\nNo Query Parameters Provided",server.getFinalString());
+	}
 
 	@Test
 	public void checkParseQueryStringReturnCorrectMap(){
@@ -70,12 +83,50 @@ public class MyHanlderTest {
 	}
 	
 	@Test
+	public void checkParseQueryStringReturnCorrectMapIfNull(){
+		MyHandler server = new MyHandler();
+		server.setFinalString(null);
+		Assert.assertEquals("{}",server.parseQueryString("").toString());
+	}
+	
+	@Test
+	public void checkParseQueryStringReturnCorrectMapIfInvalidQueryString(){
+		MyHandler server = new MyHandler();
+		server.setFinalString("&");
+		Assert.assertEquals("{}",server.parseQueryString("").toString());
+	}
+	
+	
+	
+	@Test
 	public void testhandlehasFinalString() throws Exception{
-		HttpExchange httpExchange = null;
-		MyHandler handler = mock(MyHandler.class);
-		Mockito.doNothing().when(handler).handle(httpExchange);
+		HttpExchange httpExchange = mockHttpExchange("http://localhost:8004/hello","GET","");
+		
+		MyHandler handler = new MyHandler();
 		
 		handler.handle(httpExchange);
+		Assert.assertEquals("Hello\nNo Query Parameters Provided",handler.getFinalString());
 	}
+	
+	
+	private HttpExchange mockHttpExchange(String uri, String requestMethod, String body) throws URISyntaxException, IOException {
+
+        HttpExchange httpExchange = mock(HttpExchange.class);
+
+        URI requestURI = new URI(uri);
+
+        Mockito.when(httpExchange.getRequestURI()).thenReturn(requestURI);
+        Mockito.when(httpExchange.getRequestMethod()).thenReturn(requestMethod);
+
+        if (body != null) {
+            InputStream inputStream = new ByteArrayInputStream( body.getBytes() );
+            Mockito.when(httpExchange.getRequestBody()).thenReturn(inputStream);
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Mockito.when(httpExchange.getResponseBody()).thenReturn(out);
+
+        return httpExchange;
+    }
 
 }
